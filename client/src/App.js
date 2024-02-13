ab01d871f4a8e0357b000cac4590e16fcfab6fd4
Allan Harper <Allan.Harper@snapone.com>
2024-02-13 14:21:27 -0700

    Adding socket to switch images

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,28 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import AvailableImages from './AvailableImages';
 import RotatingImage from './components/RotatingImage';
 import React from "react";
+import io from "socket.io-client";
+const socket = io.connect("http://localhost:3000");
 
 function App() {
+  function sendMessage() {
+    socket.emit("send_message", { message: "Switch image" });
+  }
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setIndex(index + 1 >= AvailableImages.images.length ? 0 : index + 1);
+    });
+  }, [socket]);
+
   const [index, setIndex] = useState(0);
 
   function handleClick() {
+    sendMessage();
     setIndex(index + 1 >= AvailableImages.images.length ? 0 : index + 1);
-    setIndex((index + 1) % AvailableImages.images.length);
+    // setIndex((index + 1) % AvailableImages.images.length);
   }
 
   let image = AvailableImages.images[index];
--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 const path = require('path');
-const express = require("express");
+const express = require('express');
+const app = express();
+const http = require('http');
+const { Server } = require("socket.io");
+const cors = require('cors');
+app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
-const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {origin:"http://localhost:3000", methods: ["GET", "POST"]},
+});
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
@@ -17,6 +25,14 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
-app.listen(PORT, () => {
+io.on("connection", (socket) => {
+  console.log(`a user connected ${socket.id}`);
+  
+  socket.on("send_message", (data) => {
+    socket.broadcast.emit("receive_message", data);
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
