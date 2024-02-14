daa316cfb59b0d9c6e90ccd0199f1502db62b8ba
Allan Harper <Allan.Harper@snapone.com>
2024-02-13 20:43:18 -0700

    Adding Selector

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,29 @@
 import './App.css';
-import { useState, useEffect } from 'react';
-import AvailableImages from './AvailableImages';
-import RotatingImage from './components/RotatingImage';
 import React from "react";
-import io from "socket.io-client";
-const socket = io.connect("/");
+import * as ReactDOM from "react-dom/client";
+import Root from "./routes/root";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Selector from './routes/selector';
 
 function App() {
-  function sendMessage() {
-    socket.emit("send_message", { message: "Switch image" });
-  }
-
-  useEffect(() => {
-    socket.on("receive_message", (data) => {
-      setIndex(index + 1 >= AvailableImages.images.length ? 0 : index + 1);
-    });
-  }, [socket]);
-
-  const [index, setIndex] = useState(0);
-
-  function handleClick() {
-    sendMessage();
-    setIndex(index + 1 >= AvailableImages.images.length ? 0 : index + 1);
-    // setIndex((index + 1) % AvailableImages.images.length);
-  }
-
-  let image = AvailableImages.images[index];
-
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => console.log(data.message));
-  }, []);
-
-
-
-  return (
-    <div className="App">
-      <RotatingImage image={image} onClick={handleClick}/>
-    </div>
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+    },
+    {
+      path: "/selector",
+      element: <Selector />,
+    },
+  ]);
+  
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -13,23 +13,36 @@ const io = new Server(server, {
     cors: {origin:"http://localhost:3000", methods: ["GET", "POST"]},
 });
 
+let index = 0;
+
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+    res.json({ message: "Hello from server!", index: index });
+});
+
+app.get("/api/increment", (req, res) => {
+    index++;
+    res.json({ message: "Index incremented!", index: index});
+});
+
+app.get("/api/set/:value", (req, res) => {
+    index = req.params.value;
+    res.json({ message: "Index set!", index: index});
 });
 
 io.on("connection", (socket) => {
   console.log(`a user connected ${socket.id}`);
   
   socket.on("send_message", (data) => {
+    if (data.index) index = data.index;
     socket.broadcast.emit("receive_message", data);
   });
 });
 
 // All other GET requests not handled before will return our React app
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
