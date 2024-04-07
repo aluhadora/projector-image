2d57c2203786c0457d723d4008b45469a8da39c5
Allan Harper <Allan.Harper@snapone.com>
2024-04-07 16:27:40 -0600

    3d Earth

--- client/src/components/MainPage.jsx
+++ client/src/components/MainPage.jsx
@@ -1,7 +1,15 @@
 import '../App.css';
 import AvailableImages from '../AvailableImages';
+import Planet from './Planet';
 import RotatingImage from './RotatingImage';
 import React from "react";
+
+function ComponentFromType({image, size, speed, classnames}) {
+  if (!image.type) {
+    return <RotatingImage image={image} size="fullSize" classNames={classnames}/>
+  }
+  if (image.type === "planet") return <Planet image={image} speed={speed} />;
+}
 
 function MainPage({state}) {
 
@@ -11,10 +19,11 @@ function MainPage({state}) {
   let fadingTimer = AvailableImages.fadingTimer[state.fadingTimer - 1] || {};
 
   let classNames = [speed.className, "fullSize", brightness.className, fadingTimer.className];
+  
 
   return (
     <div className="App">
-      <RotatingImage image={image} size="fullSize" classNames={classNames}/>
+      <ComponentFromType image={image} size="fullSize" speed={speed} classnames={classNames}/>
     </div>
   );
 }
---
+++ client/src/components/Planet/index.jsx
@@ -0,0 +1,11 @@
+import { useCallback } from 'react';
+import { mount } from './3d2';
+import './index.css';
+    
+export default function Planet({ image, speed }) {
+
+    console.log("Planet", image, speed);
+    const containerRef = useCallback((c) => mount(c, speed.rotationTicks, image.src), [speed]);
+	return <div className="Cube-container" ref={containerRef}></div>
+
+}
