202d6afaa00346f2d42bdded7206205ef2b96c46
Allan Harper <Allan.Harper@snapone.com>
2024-04-07 20:07:56 -0600

    Adding saturn

--- client/src/OfflineApp.js
+++ client/src/OfflineApp.js
@@ -5,11 +5,7 @@ import MainPage from "./components/MainPage";
 import Selector from './components/Selector/Selector';
 
 function OfflineApp() {
-  const [state, setState] = useState({});
-
-  if (!state || !state.image) {
-    setState({image: 1, speed: 1, brightness: 1, fadingTimer: 1, showSelector: false});
-  }
+  const [state, setState] = useState({image: 1, speed: 1, brightness: 1, fadingTimer: 1, showSelector: false});
 
   function sendMessage(data) {
     setState({...state, ...data});
--- client/src/components/Planet/index.jsx
+++ client/src/components/Planet/index.jsx
@@ -1,11 +1,11 @@
 import { useCallback } from 'react';
-import { mount } from './3d2';
+import { mount } from './saturn';
 import './index.css';
     
 export default function Planet({ image, speed }) {
 
     console.log("Planet", image, speed);
-    const containerRef = useCallback((c) => mount(c, speed.rotationTicks, image.src), [speed, image]);
+    const containerRef = useCallback((c) => mount(c, speed.rotationTicks, image), [speed, image]);
 	return <div className="Cube-container" ref={containerRef}></div>
 
 }
