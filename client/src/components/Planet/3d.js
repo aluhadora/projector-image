4e3cf3cdf62d5f32ec511c9ad53dbd12ecb5dac8
Allan Harper <Allan.Harper@snapone.com>
2024-05-07 11:35:16 -0600

    A couple minor fixes

--- client/src/components/Planet/3d.js
+++ client/src/components/Planet/3d.js
@@ -46,6 +46,7 @@ function init() {
 	stats = new Stats()
 	stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
 	document.body.appendChild(stats.dom)
+	stats.dom.style.display = 'none';
 }
 
 function animation( time ) {
