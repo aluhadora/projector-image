d07181c77c3f759f70713bcea36b22632798d967
Allan Harper <Allan.Harper@snapone.com>
2024-08-23 15:42:26 -0600

    scrolling weights

--- client/src/components/Yarn/colorWeights.jsx
+++ client/src/components/Yarn/colorWeights.jsx
@@ -4,7 +4,7 @@ import "./Yarn.css"
 function ColorLine({color, flowers}) {
     const totalCount = flowers.filter(f => f.petalColor === color.name).length;
     const uncompletedCount = flowers.filter(f => f.petalColor === color.name && !f.complete).length;
-    return <div>
+    return <div style={{width: 800}}>
         <span className="weightField">{color.name} </span>
         <span className="weightField"> {color.weight.toFixed(2)}</span>
         <span className="weightField">{uncompletedCount}/{totalCount}</span>
@@ -13,7 +13,7 @@ function ColorLine({color, flowers}) {
 }
 
 function LegendLine() {
-    return <div style={{marginBottom: "5px"}}>
+    return <div style={{marginBottom: "5px", width: 800}}>
         <span className="weightField">Color</span>
         <span className="weightField">Weight</span>
         <span className="weightField">Working/Total</span>
@@ -25,7 +25,7 @@ function ColorLines({colors, showWeights, setShowWeights, flowers}) {
     var lines = colors.sort((a,b) => b.weight - a.weight).map(c => <ColorLine color={c} flowers={flowers} />);
 
 
-    return <div className="section">
+    return <div className="scrollableSection">
         <h1 onClick={() => setShowWeights(!showWeights)}>Color Weights</h1>
         {showWeights && <LegendLine />}
         {showWeights && lines}
